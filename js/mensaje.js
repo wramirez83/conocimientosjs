b9640567179163150079360bfc8fb7b227e9a6d0
Wilson Ramirez Zuluaga <wilson.rz@gmail.com>
2020-11-23 10:53:08 -0500

    DOM

--- js/mensaje.js
+++ js/mensaje.js
@@ -1,1 +1,12 @@
-var name = prompt("Ingrese su Nombre");
+var name = prompt("Ingrese Su Nombre");
+if(name != null){
+    document.getElementById("nameUser").innerHTML = "Bienvenido: " + name;
+}
+
+function rename(){
+    var name = prompt("Ingrese Su Nombre");
+    if(name != null){
+    document.getElementById("nameUser").innerHTML = "Bienvenido: " + name;
+    }
+}
+
